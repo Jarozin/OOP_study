b64e320c7eef162b0e069857c79f81214f836a9e
Spirin Mihail <misha_spirin_90@mail.ru>
2023-02-27 03:40:17 +0300

    implemented actual polymorphism

--- main.cpp
+++ main.cpp
@@ -4,13 +4,10 @@
 #include "ModelDuck.h"
 int main()
 {
-    Duck::Duck *my_duck;
-    Duck::MallardDuck mallarduck;
-    my_duck = &mallarduck;
+    Duck::Duck *my_duck = new Duck::MallardDuck();
     my_duck->display();
     my_duck->PerformFly();
-    Duck::ModelDuck model_duck;
-    my_duck = &model_duck;
+    my_duck = new Duck::ModelDuck();
     my_duck->display();
     my_duck->PerformFly();
     return 0;
